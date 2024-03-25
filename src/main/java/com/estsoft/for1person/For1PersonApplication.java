1e8c8bf50591e121b4fc31422f079b6368f941f5
allewis <allewis0201@gmail.com>
2024-03-25 15:47:27 +0900

    2024-03-25 프로젝트 기본 세팅 커밋

---
+++ src/main/java/com/estsoft/for1person/For1PersonApplication.java
@@ -0,0 +1,13 @@
+package com.estsoft.for1person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class For1PersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(For1PersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estsoft/for1person/For1PersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft.for1person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class For1PersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
