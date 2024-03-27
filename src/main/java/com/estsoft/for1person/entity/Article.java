1af266992873932766518034a2b4dcef6043c154
JongHo Park <jhpark3405@gmail.com>
2024-03-28 00:22:56 +0900

    feat : Add article entity
    일반 게시판 article 및 comment 구성요소 추가

---
+++ src/main/java/com/estsoft/for1person/entity/Article.java
@@ -0,0 +1,44 @@
+package com.estsoft.for1person.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long articleId; // 게시글 아이디 (PK)
+
+    @Column(nullable = false)
+    private String title; // 제목
+
+    @Column(nullable = false)
+    private String content; // 내용
+
+    @Column(nullable = false)
+    private long views; // 조회수
+
+    @Column(nullable = false)
+    private Boolean anonymous; // 익명 여부
+
+    @Column(nullable = false)
+    private Timestamp createdAt; // 생성 날짜
+
+    @Column(nullable = false)
+    private int need; // 필요
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_writer_id")
+    private Users users; // 작성자 정보 (FK)
+
+}
+
+
---
+++ src/main/java/com/estsoft/for1person/entity/ArticleLike.java
@@ -0,0 +1,26 @@
+package com.estsoft.for1person.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class ArticleLike{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long articleLikeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    Users users;
+}
+
