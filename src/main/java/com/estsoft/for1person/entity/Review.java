a364f4d668f2ee37943b728683e04b7c2e8633f6
JongHo Park <jhpark3405@gmail.com>
2024-03-28 00:25:11 +0900

    feat : Add review entity
    리뷰 게시판 article 및 comment 구성요소 추가

---
+++ src/main/java/com/estsoft/for1person/entity/Review.java
@@ -0,0 +1,42 @@
+package com.estsoft.for1person.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Long views;
+
+    @Column(nullable = false)
+    private Boolean anonymous;
+
+    @Column(nullable = false)
+    private Timestamp createdAt;
+
+    @Column(nullable = false)
+    private Integer need;
+
+    @ManyToOne
+    @JoinColumn(name = "review_writer_id")
+    private Users users;
+
+}
