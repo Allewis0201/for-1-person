31e789d9aa3fc4205555d9e92d24e43159268d7a
euna kwon <eunakwon@naver.com>
2024-03-28 17:23:34 +0900

    docs : 회원가입 페이지 새로 지정
    회원가입 기능 테스트 용도 html에서 정식 html로 경로를 이동함

--- src/main/java/com/estsoft/for1person/controller/UserViewController.java
+++ src/main/java/com/estsoft/for1person/controller/UserViewController.java
@@ -8,7 +8,7 @@ public class UserViewController {
 
     @GetMapping("/login")
     public String login() {
-        return "/login";
+        return "login2";
     }
 
     @GetMapping("/membership")
