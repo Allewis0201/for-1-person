1102283c8dd2e469d2489172582cdce19c02952f
allewis <allewis0201@gmail.com>
2024-03-30 20:51:15 +0900

    refactor : DTO 추가
    일반 게시판, 리뷰 게시판, vip 게시판 DTO 추가

---
+++ src/main/java/com/estsoft/for1person/dto/AddReviewRequest.java
@@ -0,0 +1,18 @@
+package com.estsoft.for1person.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddReviewRequest {
+    private String title;
+    private String content;
+    private Long view;
+    private Boolean anonymous;
+    private Integer need;
+}
