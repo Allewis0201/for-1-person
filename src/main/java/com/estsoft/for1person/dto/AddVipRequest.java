f51ba915cf2429a02383d00f005f3fd6d17e0f09
allewis <allewis0201@gmail.com>
2024-04-01 11:14:49 +0900

    feat : 빌더, 엔티티 반환 함수 추가
    DTO에 빌더와 엔티티 반환 함수를 추가

--- src/main/java/com/estsoft/for1person/dto/AddVipRequest.java
+++ src/main/java/com/estsoft/for1person/dto/AddVipRequest.java
@@ -1,18 +1,41 @@
 package com.estsoft.for1person.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.estsoft.for1person.entity.Article;
+import com.estsoft.for1person.entity.User;
+import com.estsoft.for1person.entity.Vip;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+
 public class AddVipRequest {
     private String title;
     private String content;
-    private Long view;
+    private Long views;
     private Boolean anonymous;
     private Integer need;
+
+
+    @Builder
+    public AddVipRequest(String title, String content, Long views, Boolean anonymous, Integer need)
+    {
+        this.title = title;
+        this.content = content;
+        this.views = views;
+        this.anonymous = anonymous;
+        this.need = need;
+    }
+
+    public Vip toEntity(User user) // 생성자를 사용해 객체 생성
+    {
+        return Vip.builder()
+                .title(title)
+                .content(content)
+                .views(views)
+                .anonymous(anonymous)
+                .need(need)
+                .user(user)
+                .build();
+    }
 }
