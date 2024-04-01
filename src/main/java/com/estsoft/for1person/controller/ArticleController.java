6b7219498ef36c9a62a895fb4b3a6a860e786981
allewis <allewis0201@gmail.com>
2024-04-01 10:27:28 +0900

    refactor : api url 변경 및 Wrapper 타입으로 변경
    api url 변경 및 변수 타입을 Wrapper 타입으로 변경

--- src/main/java/com/estsoft/for1person/controller/ArticleController.java
+++ src/main/java/com/estsoft/for1person/controller/ArticleController.java
@@ -23,75 +23,75 @@ public class ArticleController {
     // 유저 정보 관리를 어떻게 하는지에 따라서 달라짐 (user_id가 필요 없을 수도)
 
     // 모든 목록 출력
-    @GetMapping("/common")
+    @GetMapping("/api/common")
     public ResponseEntity<List<Article>> getAllArticle() {
         return ResponseEntity.ok().body(articleService.getAllArticle());
     }
-    @GetMapping("/review")
+    @GetMapping("/api/review")
     public ResponseEntity<List<Review>> getAllReview() {
         return ResponseEntity.ok().body(articleService.getAllReview());
     }
-    @GetMapping("/vip")
+    @GetMapping("/api/vip")
     public ResponseEntity<List<Vip>> getAllVip() {
         return ResponseEntity.ok().body(articleService.getAllVip());
     }
     //============================================================================================================
     // 하나의 아티클 상세 보기
-    @GetMapping("/common/{article_id}")
-    public ResponseEntity<Article> getArticle(@PathVariable("article_id") long articleId) {
+    @GetMapping("/api/common/{article_id}")
+    public ResponseEntity<Article> getArticle(@PathVariable("article_id") Long articleId) {
         return ResponseEntity.ok().body(articleService.getArticle(articleId));
     }
-    @GetMapping("/review/{article_id}")
-    public ResponseEntity<Review> getReview(@PathVariable("article_id") long articleId) {
+    @GetMapping("/api/review/{article_id}")
+    public ResponseEntity<Review> getReview(@PathVariable("article_id") Long articleId) {
         return ResponseEntity.ok().body(articleService.getReview(articleId));
     }
-    @GetMapping("/vip/{article_id}")
-    public ResponseEntity<Vip> getVip(@PathVariable("article_id") long articleId) {
+    @GetMapping("/api/vip/{article_id}")
+    public ResponseEntity<Vip> getVip(@PathVariable("article_id") Long articleId) {
         return ResponseEntity.ok().body(articleService.getVip(articleId));
     }
     //==================================================================================================================
     // 아티클 생성(글 쓰기)
     // 아티클 내용을 받을 DTO 필요
-    @PostMapping("/common/{user_id}")
-    public void createArticle(@PathVariable("user_id") long userId) {
+    @PostMapping("/api/common/{user_id}")
+    public void createArticle(@PathVariable("user_id") Long userId) {
         articleService.createArticle(userId);
     }
-    @PostMapping("/review/{user_id}")
-    public void createReview(@PathVariable("user_id") long userId) {
+    @PostMapping("/api/review/{user_id}")
+    public void createReview(@PathVariable("user_id") Long userId) {
         articleService.createReview(userId);
     }
-    @PostMapping("/vip/{user_id}")
-    public void createVip(@PathVariable("user_id") long userId) {
+    @PostMapping("/api/vip/{user_id}")
+    public void createVip(@PathVariable("user_id") Long userId) {
         articleService.createVip(userId);
     }
     //==================================================================================================================
     // 아티클 수정
     // 수정 내용을 담을 DTO 필요
-    @PostMapping("/common/{user_id}/{article_id}")
-    public void updateArticle(@PathVariable("user_id") long userId, @PathVariable("article_id") long articleId) {
+    @PostMapping("/api/common/{user_id}/{article_id}")
+    public void updateArticle(@PathVariable("user_id") Long userId, @PathVariable("article_id") Long articleId) {
         articleService.updateArticle(userId, articleId);
     }
-    @PostMapping("/review/{user_id}/{article_id}")
-    public void updateReview(@PathVariable("user_id") long userId, @PathVariable("article_id") long articleId) {
+    @PostMapping("/api/review/{user_id}/{article_id}")
+    public void updateReview(@PathVariable("user_id") Long userId, @PathVariable("article_id") Long articleId) {
         articleService.updateReview(userId, articleId);
     }
 
-    @PostMapping("/vip/{user_id}/{article_id}")
-    public void updateVip(@PathVariable("user_id") long userId, @PathVariable("article_id") long articleId) {
+    @PostMapping("/api/vip/{user_id}/{article_id}")
+    public void updateVip(@PathVariable("user_id") Long userId, @PathVariable("article_id") Long articleId) {
         articleService.updateVip(userId, articleId);
     }
 
     // 아티클 삭제
-    @DeleteMapping("/common/{user_id}/{article_id}")
+    @DeleteMapping("/api/common/{user_id}/{article_id}")
     public void deleteArticle(@PathVariable("user_id") long userId, @PathVariable("article_id") long articleId, @PathVariable String article_id) {
         articleService.deleteArticle(userId, articleId);
     }
-    @DeleteMapping("/review/{user_id}/{article_id}")
+    @DeleteMapping("/api/review/{user_id}/{article_id}")
     public void deleteReview(@PathVariable("user_id") long userId, @PathVariable("article_id") long articleId, @PathVariable String article_id) {
         articleService.deleteReview(userId, articleId);
     }
 
-    @DeleteMapping("/vip/{user_id}/{article_id}")
+    @DeleteMapping("/api/vip/{user_id}/{article_id}")
     public void deleteVip(@PathVariable("user_id") long userId, @PathVariable("article_id") long articleId, @PathVariable String article_id) {
         articleService.deleteVip(userId, articleId);
     }
