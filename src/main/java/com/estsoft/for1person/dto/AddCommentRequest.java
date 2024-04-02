f88aa67ab8586c6404a814bf52a99776464d6cd3
allewis <allewis0201@gmail.com>
2024-04-02 11:27:35 +0900

    feat : 빌더 및 엔티티로 변환 함수 추가
    3가지 게시판 엔티티로 객체를 반환하는 함수 3개와 빌더를 추가함

--- src/main/java/com/estsoft/for1person/dto/AddCommentRequest.java
+++ src/main/java/com/estsoft/for1person/dto/AddCommentRequest.java
@@ -1,15 +1,49 @@
 package com.estsoft.for1person.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.estsoft.for1person.entity.*;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class AddCommentRequest {
     private String body;
     private Boolean anonymous;
+
+    @Builder
+    public AddCommentRequest(String body, Boolean anonymous)
+    {
+        this.body = body;
+        this.anonymous = anonymous;
+    }
+
+    public CommentCommon toCommonEntity(User user, Article article)
+    {
+        return CommentCommon.builder()
+                .user(user)
+                .article(article)
+                .body(body)
+                .anonymous(anonymous)
+                .build();
+    }
+
+    public CommentReview toReviewEntity(User user, Review review)
+    {
+        return CommentReview.builder()
+                .user(user)
+                .review(review)
+                .body(body)
+                .anonymous(anonymous)
+                .build();
+    }
+
+    public CommentVip toVipEntity(User user, Vip vip)
+    {
+        return CommentVip.builder()
+                .user(user)
+                .vip(vip)
+                .body(body)
+                .anonymous(anonymous)
+                .build();
+    }
 }
