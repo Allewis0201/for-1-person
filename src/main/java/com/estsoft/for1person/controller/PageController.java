9a5a0844a51a71dc1160e2dffd4002e31d734ad2
allewis <allewis0201@gmail.com>
2024-04-04 11:19:28 +0900

    feat : 일반, 리뷰 게시판 뷰 컨트롤러 추가
    일반, 리뷰 게시판에 연결되는 함수 추가

--- src/main/java/com/estsoft/for1person/controller/PageController.java
+++ src/main/java/com/estsoft/for1person/controller/PageController.java
@@ -1,8 +1,20 @@
 package com.estsoft.for1person.controller;
 
 
+import com.estsoft.for1person.dto.CommonViewResponse;
+import com.estsoft.for1person.dto.ReviewViewResponse;
+import com.estsoft.for1person.entity.Article;
+import com.estsoft.for1person.entity.Review;
+import com.estsoft.for1person.entity.User;
+import com.estsoft.for1person.repository.UserRepository;
 import com.estsoft.for1person.service.*;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PageController {
@@ -12,11 +24,47 @@ public class PageController {
     private CommentService commentService;
     private UserService userService;
 
-    public PageController(ArticleService articleService, ReviewService reviewService, VipService vipService, CommentService commentService, UserService userService) {
+    private UserRepository userRepository;
+
+    public PageController(ArticleService articleService, ReviewService reviewService, VipService vipService, CommentService commentService, UserService userService, UserRepository userRepository) {
         this.articleService = articleService;
         this.reviewService = reviewService;
         this.vipService = vipService;
         this.commentService = commentService;
         this.userService = userService;
+        this.userRepository = userRepository;
     }
+
+
+    @GetMapping("/commons")
+    public String getCommons(Model model, Authentication authentication)
+    {
+        List<CommonViewResponse> articles = articleService.getAllArticle().stream()
+                .map(Article::toViewResponse)
+                .toList();
+
+        String username = authentication.getName();
+        Optional<User> user = userRepository.findByUserId(username);
+        model.addAttribute("user", user.get());
+        model.addAttribute("list",articles);
+
+        return "bulletinboard";
+    }
+
+    @GetMapping("/reviews")
+    public String getReviews(Model model, Authentication authentication)
+    {
+        List<ReviewViewResponse> articles = articleService.getAllReview().stream()
+                .map(Review::toViewResponse)
+                .toList();
+
+        String username = authentication.getName();
+        Optional<User> user = userRepository.findByUserId(username);
+        model.addAttribute("user", user.get());
+        model.addAttribute("list",articles);
+
+        return "review-board";
+    }
+
+
 }
