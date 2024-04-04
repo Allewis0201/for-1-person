c9121d627b5ddad2b0d26983af434119e72761db
allewis <allewis0201@gmail.com>
2024-04-04 11:18:26 +0900

    feat : view response 추가
    페이지 작업을 처리하기 위해 view response 추가

---
+++ src/main/java/com/estsoft/for1person/dto/ReviewViewResponse.java
@@ -0,0 +1,24 @@
+package com.estsoft.for1person.dto;
+
+import com.estsoft.for1person.entity.User;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewViewResponse {
+
+    private Long articleId; // 게시글 아이디 (PK)
+    private String title; // 제목
+    private String content; // 내용
+    private Long views; // 조회수
+    private Boolean anonymous; // 익명 여부
+    private Timestamp createdAt; // 생성 날짜
+    private Integer need; // 필요
+    private User user; // 작성자 정보 (FK)
+    private Integer score;
+}
