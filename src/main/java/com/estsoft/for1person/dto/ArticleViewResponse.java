9d536daa1e9a78251be23029de19cf48ed7c6a06
allewis <allewis0201@gmail.com>
2024-04-04 17:18:22 +0900

    refactor : 중복 부분 제거
    각자 작업 후 머지 했을 때 더 이상 사용하지 않는 코드가 생겨 제거함

--- src/main/java/com/estsoft/for1person/dto/ArticleViewResponse.java
+++ src/main/java/com/estsoft/for1person/dto/ArticleViewResponse.java
@@ -1,48 +1,24 @@
 package com.estsoft.for1person.dto;
 
 import com.estsoft.for1person.entity.Article;
+import com.estsoft.for1person.entity.User;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ArticleViewResponse {
     private Long articleId;
     private String title;
     private String content;
-    private LocalDateTime createdAt;
     private Long views;
     private Boolean anonymous;
-    private String authorName;
+    private LocalDateTime createdAt;
+    private Integer need;
+    private User user;
 
-    public ArticleViewResponse(){
-    }
-
-    public Long getArticleId() {
-        return articleId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public ArticleViewResponse(Article article){
-        this.articleId = article.getArticleId();
-        this.title = article.getTitle();
-        this.content = article.getContent();
-        if (article.getCreatedAt() != null) {
-            this.createdAt = article.getCreatedAt().toLocalDateTime();
-        } else {
-            this.createdAt = null;
-        }
-        this.views = article.getViews();
-        this.anonymous = article.getAnonymous();
-        this.createdAt = article.getCreatedAt().toLocalDateTime();
-    }
 }
--- src/main/java/com/estsoft/for1person/dto/ReviewViewResponse.java
+++ src/main/java/com/estsoft/for1person/dto/ReviewViewResponse.java
@@ -17,43 +17,10 @@ public class ReviewViewResponse {
     private Long reviewId;
     private String title;
     private String content;
-    private LocalDateTime createdAt;
     private Long views;
     private Boolean anonymous;
-    private String authorName;
+    private LocalDateTime createdAt;
     private Integer need;
     private Integer score;
-
-    public ReviewViewResponse(){
-    }
-
-    public Long getReviewId() {
-        return reviewId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public ReviewViewResponse(Review review){
-        this.reviewId = review.getReviewId();
-        this.title = review.getTitle();
-        this.content = review.getContent();
-        if (review.getCreatedAt() != null) {
-            this.createdAt = review.getCreatedAt().toLocalDateTime();
-        } else {
-            this.createdAt = null;
-        }
-        this.views = review.getViews();
-        this.anonymous = review.getAnonymous();
-        this.createdAt = review.getCreatedAt().toLocalDateTime();
-    }
+    private User user;
 }
