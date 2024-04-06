69163cb3f2cafeae91f5ffeba13a9333f2d76863
euna kwon <eunakwon@naver.com>
2024-04-06 16:41:35 +0900

    feat : Vip 게시판 댓글 추가 기능
    #90

---
+++ src/main/resources/static/js/detailVip.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('commentVipForm');
+
+    form.onsubmit = function(e) {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        const mapping = {};
+        formData.forEach((value, key) => {
+            mapping[key] = value;
+        });
+
+        fetch(form.action, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(mapping)
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.redirectUrl) {
+                    window.location.href = data.redirectUrl;
+                }
+            })
+            .catch(error => console.error('Error:', error));
+
+    };
+});
