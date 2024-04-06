88262280f21cdc7c72a286eda209e0513273381c
allewis <allewis0201@gmail.com>
2024-04-06 13:49:44 +0900

    feat : 좋아요 기능, 좋아요 반환 기능 분리
    합쳐져 있던 2개의 기능을 서로 다른 함수로 분리함
    
    #55

--- src/main/java/com/estsoft/for1person/controller/ArticleController.java
+++ src/main/java/com/estsoft/for1person/controller/ArticleController.java
@@ -129,22 +129,41 @@ public class ArticleController {
     //==================================================================================================================
     //게시글 좋아요 기능, 이미 좋아요 되어 있으면 좋아요 취소
     @GetMapping("/api/common/like/{user_id}/{article_id}")
-    public Integer likeCommonArticle(@PathVariable("user_id") String user_id, @PathVariable("article_id") Long article_id) {
-        Integer result = articleService.likeArticle(user_id, article_id).get();
+    public void likeCommonArticle(@PathVariable("user_id") String user_id, @PathVariable("article_id") Long article_id) {
+        articleService.likeArticle(user_id, article_id);
 
-        return result;
+
     }
     @GetMapping("/api/review/like/{user_id}/{article_id}")
-    public Integer likeReviewArticle(@PathVariable("user_id") String user_id, @PathVariable("article_id") Long article_id) {
-        Integer result = articleService.likeReview(user_id, article_id).get();
+    public void likeReviewArticle(@PathVariable("user_id") String user_id, @PathVariable("article_id") Long article_id) {
+        articleService.likeReview(user_id, article_id);
 
-        return result;
+
     }
     @GetMapping("/api/vip/like/{user_id}/{article_id}")
-    public Integer likeVipArticle(@PathVariable("user_id") String user_id, @PathVariable("article_id") Long article_id) {
-        Integer result = articleService.likeVip(user_id, article_id).get();
-
-        return result;
+    public void likeVipArticle(@PathVariable("user_id") String user_id, @PathVariable("article_id") Long article_id) {
+        articleService.likeVip(user_id, article_id);
     }
+
+    @GetMapping("/api/common/like/{article_id}")
+    public Integer getLikeCommonArticle(@PathVariable("article_id") Long article_id)
+    {
+        return articleService.getLikeArticle(article_id).get();
+    }
+
+    @GetMapping("/api/review/like/{article_id}")
+    public Integer getLikeReviewArticle(@PathVariable("article_id") Long article_id)
+    {
+        return articleService.getLikeReview(article_id).get();
+    }
+
+
+    @GetMapping("/api/vip/like/{article_id}")
+    public Integer getLikeVipArticle(@PathVariable("article_id") Long article_id)
+    {
+        return articleService.getLikeVip(article_id).get();
+    }
+
+
 
 }
